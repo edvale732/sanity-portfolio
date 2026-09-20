1210d03af122fee0298a66103366818c1a41924e
edvale732 <edwardvale73@outlook.com>
2026-09-20 16:57:21 +0100

    updated sanity frontend config to allow for visual editing

--- apps/web/src/sanity/client.ts
+++ apps/web/src/sanity/client.ts
@@ -3,6 +3,9 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   projectId: "l33zytkl",
   dataset: "production",
-  apiVersion: "2025-07-09",
-  useCdn: false,
+  apiVersion: '2026-02-01',
+  useCdn: true,
+  stega: {
+    studioUrl: 'http://localhost:3333',
+  },
 });
