978831fcf696ea18b7ef52d8849b4fdd1bb8d3a2
edvale732 <edwardvale73@outlook.com>
2026-09-20 16:54:47 +0100

    added presentation tool to sanity

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
+import {presentationTool} from 'sanity/presentation'
 import {schemaTypes} from './schemaTypes'
 import {structure} from './structure' 
 
@@ -11,7 +12,17 @@ export default defineConfig({
   projectId: 'l33zytkl',
   dataset: 'production',
 
-  plugins: [structureTool({structure}), visionTool()],
+  plugins: [structureTool({structure}), 
+    visionTool(), 
+    presentationTool({
+      resolve,
+      previewUrl: {
+        initial: 'http://localhost:3000',
+        previewMode: {
+          enable: '/api/draft-mode/enable',
+        },
+      },
+    }),],
 
   schema: {
     types: schemaTypes,
