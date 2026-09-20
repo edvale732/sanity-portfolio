8cd89fdcdfeadc90fcc27e8187bc33f103ac849e
edvale732 <edwardvale73@outlook.com>
2026-09-20 15:27:33 +0100

    created projectType schema

---
+++ apps/studio/schemaTypes/projectType.tsx
@@ -0,0 +1,89 @@
+import {defineField, defineType} from 'sanity'
+
+export const projectType = defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+      group: 'details',
+      
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      group: 'details',
+      options: {source: 'name'},
+      validation: (rule) => rule
+      .required()
+      .error('Slug is required to generate a URL on the website'),
+    }),
+    defineField({
+      name: 'projectType',
+      type: 'array',
+      group: 'details',
+      of: [{type: 'string', options: {list: ['director', 'producer', 'actor', 'editor'], layout: 'radio'}}],
+      validation: (rule) => rule
+      .required()
+      .error('Project type is required'),
+    
+    }),    
+    defineField({
+      name: 'date',
+      type: 'date',
+      group: 'details',
+      validation: (rule) => rule
+      .required()
+      .error('Date is required for the project')
+      .info('This is the date of the project')
+    }),
+    defineField({
+      name: 'description',
+      type: 'array',
+      of: [{type: 'block'}],
+      validation: rule => 
+      rule
+        .min(100)
+        .max(200)
+        .warning('For consistency, this summary should be between 100-200 characters')
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'video',
+      type: 'url',
+    }),
+    
+    defineField({
+      name: 'tags',
+      type: 'array',
+      of: [{type: 'string'}],
+    }),
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      image: 'image',
+      date: 'date',
+    },
+    prepare({name, image, date}) {
+        const nameFormatted = name || 'Untitled Project'
+        const dateFormatted = date ? new Date(date).toLocaleString(undefined, {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric',
+
+        }) : ''
+        return {
+            title: nameFormatted,
+            subtitle: dateFormatted,
+            media: image
+        }
+    }
+  }
+})
